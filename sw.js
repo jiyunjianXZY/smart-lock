6abb7dbeed222aedd5a2a1313381c0008a1c6adc
jiyunjianXZY <jiyunjianXZY@outlook.com>
2026-09-21 14:42:56 +0800

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE = "smartlock-v2";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./open.html",
+  "./config.js",
+  "./manifest.webmanifest",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./icons/apple-touch-icon.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then((cache) => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+// 静态资源：网络优先，失败回退缓存；跨域请求（OneNET API）不拦截
+self.addEventListener("fetch", (event) => {
+  const url = new URL(event.request.url);
+  if (url.origin !== location.origin) {
+    return;
+  }
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then((m) => m || caches.match("./index.html")))
+  );
+});
